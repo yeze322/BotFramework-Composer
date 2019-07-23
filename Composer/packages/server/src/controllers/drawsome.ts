9cc7ababfe06914224a8a00b84b960cb7e93fc79
zeye <zeye@microsoft.com>
2019-07-23 15:37:48 +0800

    auto reload bot runtime

--- Composer/packages/server/src/controllers/drawsome.ts
+++ Composer/packages/server/src/controllers/drawsome.ts
@@ -2,16 +2,25 @@ import { writeFileSync } from 'fs';
 
 import { Request, Response } from 'express';
 
+import { Path } from '../utility/path';
 import settings from '../settings/settings.json';
+import BotConnectorService from '../services/connector';
 
-import { Path } from './../utility/path';
+// eslint-disable-next-line
+const syncConfig = {
+  luis: { name: '', environment: '', authoringKey: '', authoringRegion: 'westus', defaultLanguage: 'en-us' },
+  MicrosoftAppId: '',
+  MicrosoftAppPassword: '',
+};
 
 async function updateDrawsomeBot(req: Request, res: Response) {
   const folderPath = Path.resolve(settings.development.defaultFolder);
   const targetPath = `${folderPath}/DrawsomeBot/Main/Main.dialog`;
   writeFileSync(targetPath, JSON.stringify(req.body, null, '\t'));
+  await BotConnectorService.sync(syncConfig);
   res.status(200).json({
     path: targetPath,
+    synced: true,
     data: req.body,
   });
 }
