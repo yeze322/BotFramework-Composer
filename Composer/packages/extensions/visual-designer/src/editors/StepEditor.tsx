10d99aaa4df8d246fcfda6ab76cefd003853a966
zeye <zeye@microsoft.com>
2019-08-12 14:34:28 +0800

    turn StepEditor into Class Component

--- Composer/packages/extensions/visual-designer/src/editors/StepEditor.tsx
+++ Composer/packages/extensions/visual-designer/src/editors/StepEditor.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React, { useState } from 'react';
+import React from 'react';
 import { EdgeMenu } from 'shared-menus';
 
 import { StepGroup } from '../components/groups';
@@ -36,56 +36,68 @@ const TailSize = {
   height: CircleSize.height + ElementInterval.y / 2,
 };
 
-export const StepEditor = ({ id, data, onEvent }): JSX.Element => {
-  const [stepGroupBoundary, setStepGroupBoundary] = useState(measureJsonBoundary(data));
+export class StepEditor extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stepGroupBoundary: measureJsonBoundary(props.data),
+    };
+  }
 
-  const hasNoSteps = !data || !Array.isArray(data.children) || data.children.length === 0;
-  const content = hasNoSteps ? (
-    <EdgeMenu
-      onClick={$type => onEvent(NodeEventTypes.Insert, { id, $type, position: 0 })}
-      data-testid="StepGroupAdd"
-    />
-  ) : (
-    <StepGroup
-      id={id}
-      data={data}
-      onEvent={onEvent}
-      onResize={boundary => {
-        setStepGroupBoundary(boundary);
-      }}
-    />
-  );
-  const contentBoundary = hasNoSteps ? new Boundary(CircleSize.width, CircleSize.height) : stepGroupBoundary;
+  render(): JSX.Element {
+    const { id, data, onEvent } = this.props;
+    const { stepGroupBoundary } = this.state;
 
-  const editorWidth =
-    Math.min(
-      Math.max(0, HeadSize.width / 2, TailSize.width / 2, contentBoundary.axisX),
-      Math.max(0, HeadSize.width / 2, TailSize.width / 2, contentBoundary.width - contentBoundary.axisX)
-    ) * 2;
-  const editorHeight = HeadSize.height + TailSize.height + contentBoundary.height;
-  const editorAxisX = editorWidth / 2;
+    const hasNoSteps = !data || !Array.isArray(data.children) || data.children.length === 0;
+    const content = hasNoSteps ? (
+      <EdgeMenu
+        onClick={$type => onEvent(NodeEventTypes.Insert, { id, $type, position: 0 })}
+        data-testid="StepGroupAdd"
+      />
+    ) : (
+      <StepGroup
+        id={id}
+        data={data}
+        onEvent={onEvent}
+        onResize={boundary => {
+          this.setState({
+            stepGroupBoundary: boundary,
+          });
+        }}
+      />
+    );
+    const contentBoundary = hasNoSteps ? new Boundary(CircleSize.width, CircleSize.height) : stepGroupBoundary;
 
-  return (
-    <div className="step-editor" css={{ position: 'relative', width: editorWidth, height: editorHeight }}>
-      <OffsetContainer offset={{ x: editorAxisX - HeadSize.width / 2, y: 0 }}>
-        <div className="step-editor__head" css={{ ...HeadSize, position: 'relative' }}>
-          <OffsetContainer offset={{ x: 0, y: 0 }}>
-            <Trigger />
-          </OffsetContainer>
-          <Edge direction="y" x={TriggerSize.width / 2} y={TriggerSize.height} length={ElementInterval.y / 2} />
-        </div>
-      </OffsetContainer>
-      <OffsetContainer offset={{ x: editorAxisX - contentBoundary.axisX, y: HeadSize.height }}>
-        {content}
-      </OffsetContainer>
-      <OffsetContainer offset={{ x: editorAxisX - TailSize.width / 2, y: contentBoundary.height + HeadSize.height }}>
-        <div className="step-editor__tail" css={{ ...TailSize, position: 'relative' }}>
-          <Edge direction="y" x={CircleSize.width / 2} y={0} length={ElementInterval.y / 2} directed={true} />
-          <OffsetContainer offset={{ x: -1, y: ElementInterval.y / 2 }}>
-            <Circle />
-          </OffsetContainer>
-        </div>
-      </OffsetContainer>
-    </div>
-  );
-};
+    const editorWidth =
+      Math.min(
+        Math.max(0, HeadSize.width / 2, TailSize.width / 2, contentBoundary.axisX),
+        Math.max(0, HeadSize.width / 2, TailSize.width / 2, contentBoundary.width - contentBoundary.axisX)
+      ) * 2;
+    const editorHeight = HeadSize.height + TailSize.height + contentBoundary.height;
+    const editorAxisX = editorWidth / 2;
+
+    return (
+      <div className="step-editor" css={{ position: 'relative', width: editorWidth, height: editorHeight }}>
+        <OffsetContainer offset={{ x: editorAxisX - HeadSize.width / 2, y: 0 }}>
+          <div className="step-editor__head" css={{ ...HeadSize, position: 'relative' }}>
+            <OffsetContainer offset={{ x: 0, y: 0 }}>
+              <Trigger />
+            </OffsetContainer>
+            <Edge direction="y" x={TriggerSize.width / 2} y={TriggerSize.height} length={ElementInterval.y / 2} />
+          </div>
+        </OffsetContainer>
+        <OffsetContainer offset={{ x: editorAxisX - contentBoundary.axisX, y: HeadSize.height }}>
+          {content}
+        </OffsetContainer>
+        <OffsetContainer offset={{ x: editorAxisX - TailSize.width / 2, y: contentBoundary.height + HeadSize.height }}>
+          <div className="step-editor__tail" css={{ ...TailSize, position: 'relative' }}>
+            <Edge direction="y" x={CircleSize.width / 2} y={0} length={ElementInterval.y / 2} directed={true} />
+            <OffsetContainer offset={{ x: -1, y: ElementInterval.y / 2 }}>
+              <Circle />
+            </OffsetContainer>
+          </div>
+        </OffsetContainer>
+      </div>
+    );
+  }
+}
