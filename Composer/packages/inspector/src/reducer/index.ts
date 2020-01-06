d9ff8051d3ed362eae8c66ffa9b84a0a886107a8
zeye <zeye@microsoft.com>
2020-01-06 18:48:57 +0800

    improve store states relations

--- Composer/packages/inspector/src/reducer/index.ts
+++ Composer/packages/inspector/src/reducer/index.ts
@@ -4,15 +4,15 @@
 import { SET_PROJECT, SET_DIALOG, SET_TRIGGER, SET_ACTION, APPEND_LOG } from '../actions/messengerActions';
 import { initialStore, InspectorStore } from '../store';
 
-export const reducer = (store: InspectorStore = initialStore, action) => {
+export const reducer = (store: InspectorStore = initialStore, action): InspectorStore => {
   const { type, payload } = action;
   switch (type) {
     case SET_PROJECT:
-      return { ...store, project: payload };
+      return { ...store, project: payload, dialogName: '', focusedEvent: '', focusedAction: '' };
     case SET_DIALOG:
-      return { ...store, dialogName: payload };
+      return { ...store, dialogName: payload, focusedEvent: '', focusedAction: '' };
     case SET_TRIGGER:
-      return { ...store, focusedEvent: payload };
+      return { ...store, focusedEvent: payload, focusedAction: '' };
     case SET_ACTION:
       return { ...store, focusedAction: payload };
     case APPEND_LOG:
