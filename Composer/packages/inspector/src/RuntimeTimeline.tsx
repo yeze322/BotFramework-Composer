e1118d724bdf4ad506bd48691f45dc11ac85ac1c
zeye <zeye@microsoft.com>
2020-01-13 19:37:02 +0800

    beautify timeline colors

--- Composer/packages/inspector/src/RuntimeTimeline.tsx
+++ Composer/packages/inspector/src/RuntimeTimeline.tsx
@@ -2,17 +2,40 @@
 // Licensed under the MIT License.
 
 import React, { FC } from 'react';
-import { Timeline } from 'antd';
+import { Timeline, Tag } from 'antd';
 
-import { RuntimeHistory, RuntimeActivity } from './store';
+import { RuntimeHistory, RuntimeActivity, RuntimeActivityTypes } from './store';
+import { Colors } from './colors';
 
 const RuntimeActivityRenderer: React.FC<{ activity: RuntimeActivity }> = ({ activity }) => {
-  return <p>{activity.value}</p>;
+  let tagText = '',
+    tagColor = 'blue';
+  switch (activity.type) {
+    case RuntimeActivityTypes.Action:
+      tagText = 'Action';
+      tagColor = Colors.Action;
+      break;
+    case RuntimeActivityTypes.BotAsks:
+      tagText = 'BotResponse';
+      tagColor = Colors.Bot;
+      break;
+    case RuntimeActivityTypes.UserInput:
+      tagText = 'UserInput';
+      tagColor = Colors.User;
+      break;
+  }
+  return (
+    <p>
+      <Tag color={tagColor}>{tagText}</Tag>
+      {activity.value}
+    </p>
+  );
 };
 
 const RuntimeTriggerItem: React.FC<{ history: RuntimeHistory }> = ({ history }) => {
   return (
     <p>
+      <Tag color={Colors.Trigger}>Trigger</Tag>
       {history.dialog} / {history.trigger}
     </p>
   );
