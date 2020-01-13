1760d6395e64ab0f5d62899f67237e5b060867bd
zeye <zeye@microsoft.com>
2020-01-13 12:55:41 +0800

    show runtime timeline

--- Composer/packages/inspector/src/RuntimeTimeline.tsx
+++ Composer/packages/inspector/src/RuntimeTimeline.tsx
@@ -1,34 +1,37 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { Timeline } from 'antd';
 
+import { StoreContext } from './store/StoreContext';
+import { RuntimeHistory } from './store';
+
+const RuntimeHistoryCard: React.FC<{ history: RuntimeHistory }> = ({ history }) => {
+  return (
+    <div>
+      <p>
+        {history.dialog} / {history.trigger}
+      </p>
+      {history.actions.map((act, index) => (
+        <p key={`${history.dialog}/${history.trigger}[${index}]`}>{act}</p>
+      ))}
+    </div>
+  );
+};
+
 export const RuntimeTimeline = () => {
+  const { store } = useContext(StoreContext);
+  const { historys } = store;
   return (
     <Timeline>
-      <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
-      <Timeline.Item color="green">Create a services site 2015-09-01</Timeline.Item>
-      <Timeline.Item color="red">
-        <p>Solve initial network problems 1</p>
-        <p>Solve initial network problems 2</p>
-        <p>Solve initial network problems 3 2015-09-01</p>
-      </Timeline.Item>
-      <Timeline.Item>
-        <p>Technical testing 1</p>
-        <p>Technical testing 2</p>
-        <p>Technical testing 3 2015-09-01</p>
-      </Timeline.Item>
-      <Timeline.Item color="gray">
-        <p>Technical testing 1</p>
-        <p>Technical testing 2</p>
-        <p>Technical testing 3 2015-09-01</p>
-      </Timeline.Item>
-      <Timeline.Item color="gray">
-        <p>Technical testing 1</p>
-        <p>Technical testing 2</p>
-        <p>Technical testing 3 2015-09-01</p>
-      </Timeline.Item>
+      {historys.map(history => {
+        return (
+          <Timeline.Item key={`${history.dialog}/${history.trigger}`}>
+            <RuntimeHistoryCard history={history} />
+          </Timeline.Item>
+        );
+      })}
     </Timeline>
   );
 };
