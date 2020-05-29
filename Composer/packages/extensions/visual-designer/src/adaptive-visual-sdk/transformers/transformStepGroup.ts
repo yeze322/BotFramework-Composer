f2444e0f6ae2744802a8811f2113d0ab8798716c
zeye <zeye@microsoft.com>
2020-05-29 17:08:20 +0800

    fix a not updated reference

--- Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/transformers/transformStepGroup.ts
+++ Composer/packages/extensions/visual-designer/src/adaptive-visual-sdk/transformers/transformStepGroup.ts
@@ -3,11 +3,11 @@
 
 import { AdaptiveKinds } from '../constants/AdaptiveKinds';
 import { IndexedNode } from '../models/IndexedNode';
-import { normalizeObiStep } from '../utils/adaptive/stepBuilder';
+import { normalizeAdaptiveAction } from '../utils/adaptive/actionBuilder';
 
 export function transformStepGroup(input, groupId): IndexedNode[] {
   if (!input || input.$kind !== AdaptiveKinds.StepGroup) return [];
   if (!input.children || !Array.isArray(input.children)) return [];
 
-  return input.children.map((step, index) => new IndexedNode(`${groupId}[${index}]`, normalizeObiStep(step)));
+  return input.children.map((step, index) => new IndexedNode(`${groupId}[${index}]`, normalizeAdaptiveAction(step)));
 }
